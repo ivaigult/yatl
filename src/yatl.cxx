25b22af1c05d9e97dd3101e2ed20d78b158123bb
Ivan <i.vaigult@gmail.com>
2017-05-03 09:42:10 -0700

    add --help and --expr options

--- src/yatl.cxx
+++ src/yatl.cxx
@@ -24,22 +24,71 @@
 
 #include <iostream>
 #include <fstream>
+#include <sstream>
+#include <functional>
+#include <map>
+
+enum class parse_result {
+    ok,
+    stop,
+    error
+};
 
 int main(int argc, char** argv)
 {
-    const char* script_file_name = nullptr;
-    for (size_t ii = 1; ii < argc && !script_file_name; ++ii) {
-        script_file_name = argv[ii];
-    }
+    size_t arg_counter = 1;
     std::istream* istream = &std::cin;
     std::ostream* ostream = &std::cout;
     std::ifstream script_file;
+    std::string   expression;
+    std::istringstream expression_stream;
     std::ofstream dev_null;
-    if (script_file_name) {
-        script_file.open(script_file_name);
-        istream = &script_file;
-        ostream = &dev_null;
+
+    auto print_help = [&argv]() -> parse_result {
+        std::cerr << "Usage: " << argv[0] << " [script_path] [script_args]" << std::endl;
+        std::cerr << "       " << argv[0] << " [-e|--expr] expression" << std::endl;
+        return parse_result::error;
+    };
+
+    auto set_expression = [&]() {
+        const char* expression_str = argv[++arg_counter];
+        expression = expression_str? expression_str : "";
+        expression_stream.str(expression);
+        istream = &expression_stream;
+        return parse_result::stop;
+    };
+
+    std::map<std::string, std::function<parse_result(void)> > arg_parser = {   
+        {"-h",      print_help },
+        { "--help", print_help },
+        {"-e",      set_expression },
+        { "--expr", set_expression },
+    };
+
+    for (; arg_counter < argc; ++arg_counter) {
+        auto found = arg_parser.find(argv[arg_counter]);
+        if (found != arg_parser.end()) {
+            parse_result result = found->second();
+            switch (result) {
+            case parse_result::ok:
+                continue;
+            case parse_result::stop:
+                goto done_parsing;
+            case parse_result::error:
+                exit(EXIT_FAILURE);
+            }
+        } else {
+            script_file.open(argv[arg_counter]);
+            if (!script_file) {
+                std::cerr << "Unable to open " << argv[arg_counter] << std::endl;
+                exit(EXIT_FAILURE);
+            }
+            istream = &script_file;
+            ostream = &dev_null;
+            break;
+        }
     }
+done_parsing:;
 
     return yatl::repl(*istream, *ostream, std::cerr).exec();
 }
