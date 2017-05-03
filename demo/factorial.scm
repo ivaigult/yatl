15dab93fd8237f86a7eb0170ad0f75b5483fe58e
Ivan <i.vaigult@gmail.com>
2017-05-03 09:54:53 -0700

    First demo

---
+++ demo/factorial.scm
@@ -0,0 +1,20 @@
+(define factorial
+	(lambda (x)
+		(cond
+			((= x 0) 1)
+			(#t (* x (factorial (- x 1))))
+		)
+	)
+)
+
+(print (factorial 0))
+(print (factorial 1))
+(print (factorial 2))
+(print (factorial 3))
+(print (factorial 4))
+(print (factorial 5))
+(print (factorial 6))
+(print (factorial 7))
+(print (factorial 8))
+(print (factorial 9))
+(print (factorial 10))
