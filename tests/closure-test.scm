ee8b748127650d8c01ebf6fe89c147df07965e7c
Ivan <i.vaigult@gmail.com>
2017-05-29 05:26:56 -0700

    Add more tests

--- tests/closure-test.scm
+++ tests/closure-test.scm
@@ -15,4 +15,15 @@
 	(assert (= (counter 'get) 2)       "The result of two increments wasn't stored in closure")
 ))
 
+(add-test "closure-instance-are-independent" (lambda ()
+	(define counter-a (make_counter))
+	(define counter-b (make_counter))
+	(assert (= (counter-a 'get) 0)       "Initial value of counter is not null")
+	(assert (= (counter-b 'get) 0)       "Initial value of counter is not null")
+	(assert (= (counter-a 'increment) 1) "Increment did't increase the value")
+	(assert (= (counter-b 'increment) 1) "Increment did't increase the value")
+	(assert (= (counter-a 'increment) 2) "Increment did't increase the value")
+	(assert (= (counter-b 'get) 1)       "Closures shouldn't share their state")
+))
+
 (run-all-tests)
