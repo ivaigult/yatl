61c707dd036eeaebd56872fa84158157d341b022
ivaigult <i.vaigult@gmail.com>
2017-05-29 21:30:34 +0300

    Extend let-locals tests

--- tests/let-test.scm
+++ tests/let-test.scm
@@ -14,7 +14,17 @@
     )
 ))
 
-
+(add-test "let-overrides-locals" (lambda ()
+    (let ((local-x 100500))
+        (define local-x-copy local-x)
+        (let ((local-x 0))
+            (assert (= local-x 0) "Unexpected x value")
+            (assert (= 100500 local-x-copy) "Copy value should remain unchanged")
+        )
+        (assert (= local-x 100500) "But value should be restored")
+        (assert (= 100500 local-x-copy) "Copy value should remain unchanged")
+    )
+))
 
 
 (run-all-tests)
