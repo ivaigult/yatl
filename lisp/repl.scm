db2b228611ab1c1aa6c8d919715aeae6ae55d44f
Ivan Vaigult <i.vaigult@gmail.com>
2017-12-08 11:22:53 +0000

    Move repl logic to closure

--- lisp/repl.scm
+++ lisp/repl.scm
@@ -1,26 +1,28 @@
-(define std-in (create-console-input-port))
-(define std-out (create-console-output-port))
-
-(define (repl-iter)
-    (output-port-write-string std-out "yatl> ")
-    (let* ((read-obj (input-port-read std-in))
-           (eval-obj (eval read-obj)))
-        (cond ((input-port-eof? std-in) (exit 0)))
-        (output-port-write-string std-out "$     ")
-        (output-port-write std-out eval-obj)
+(define (repl)
+    (define std-in (create-console-input-port))
+    (define std-out (create-console-output-port))
+    (define (repl-iter)
+        (output-port-write-string std-out "yatl> ")
+        (let* ((read-obj (input-port-read std-in))
+               (eval-obj (eval read-obj)))
+            (cond ((input-port-eof? std-in) (exit 0)))
+            (output-port-write-string std-out "$     ")
+            (output-port-write std-out eval-obj)
+            (output-port-newline std-out)
+        )
+    )
+    (define (repl-on-error err)
+        (output-port-write std-out err)
         (output-port-newline std-out)
     )
-)
-
-(define (repl-on-error err)
-    (output-port-write std-out err)
-    (output-port-newline std-out)
-)
-
-(forever
-    (with-exception-handler
-        repl-on-error
-        repl-iter
+    (define (repl-exec)
+        (forever
+            (with-exception-handler
+                repl-on-error
+	        repl-iter))
     )
+    repl-exec
 )
+
+((repl))
 
